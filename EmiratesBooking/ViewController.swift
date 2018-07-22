ccd775d71fcbfc5b40575d1714f0ea4a6085790d
rafal.manka <rafal@manka.info>
2018-07-22 11:17:02 +0400

    Initial Commit

---
+++ EmiratesBooking/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EmiratesBooking
+//
+//  Created by rafal.manka on 22/07/2018.
+//  Copyright © 2018 Emirates Airlines. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
